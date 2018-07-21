e6397b66f4062575527849a5e0c5b3b21106650d
Matthew Gentzkow <gentzkow@stanford.edu>
2018-07-20 17:14:29 -0700

    Continue to play w/ repo structure

--- setup.py
+++ setup.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python
 
-
-    # Determines whether to print traceback messages
-
-    # Copies config_user template if no such file exists
-
-    # Loads config yaml files
-
-    # Stores executable names and prerequisite checks. Prefer user to global.
-
-    # Checks prerequisite applications and record known prereqs
-
-    # Stores PYTHONPATH
-    
-    # Gets return list & returns
+# Check that gslab_make library is correctly installed
+# Correct that other software packages are correctly installed
+# Install dependencies for Python, R, Stata
+# Check that config_local.yaml exists; create it from template if it does not
+# Check that config_local.yaml and config.yaml parse correctly
+# Check that paths to all external resources are valid
 
