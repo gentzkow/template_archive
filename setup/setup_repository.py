c81917e3b1537ebd2b852e7e48345c70d77e8b04
Zong Huang <zong@sr16-7d74d5d942.stanford.edu>
2018-12-19 11:52:27 -0800

    Update path to look for gslab_make for #10

--- setup/setup_repository.py
+++ setup/setup_repository.py
@@ -13,7 +13,7 @@ except:
     raise Exception
 
 ROOT = git.Repo('.', search_parent_directories = True).working_tree_dir
-f, path, desc = imp.find_module('gslab_make', [os.path.join(ROOT, 'lib', 'gslab_make')])
+f, path, desc = imp.find_module('gslab_make', [os.path.join(ROOT, 'lib')])
 gs = imp.load_module('gslab_make', f, path, desc)
 
 # GENERAL
