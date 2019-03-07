b3b50d4e7a89228355801912438d90279bdf6e7f
Zong Huang <z-y-huang@github.com>
2019-03-07 15:26:27 -0800

    Add comments to code for #16

--- data/make.py
+++ data/make.py
@@ -1,55 +1,56 @@
-# ENVIRONMENT
+### ENVIRONMENT
 import git
 import imp
 import os
 import yaml
 
-ROOT = git.Repo('.', search_parent_directories = True).working_tree_dir
-f, path, desc = imp.find_module('gslab_make', [os.path.join(ROOT, 'lib')])
+ROOT = git.Repo('.', search_parent_directories = True).working_tree_dir 
+f, path, desc = imp.find_module('gslab_make', [os.path.join(ROOT, 'lib')]) 
 gs = imp.load_module('gslab_make', f, path, desc)
 
-PATHS = {
+PATHS = { # Set default paths; used by `gslab_make` functions
     'config_user'     : '../config_user.yaml',
     'input_dir'       : 'input', 
     'external_dir'    : 'external',
     'output_dir'      : 'output/',
+	'output_dir_local': [],                     # Optional; include any local directories with outputs
     'pdf_dir'         : 'output/',
-    'makelog'         : 'log/make.log',
-    'output_statslog' : 'log/output_stats.log',
-    'output_headslog' : 'log/output_heads.log', 
-    'link_maplog'     : 'log/link_map.log',
-    'link_statslog'   : 'log/link_stats.log',
-    'link_headslog'   : 'log/link_heads.log'
+    'makelog'         : 'log/make.log',         # Set to '' to avoid writing log
+    'output_statslog' : 'log/output_stats.log', # Set to '' to avoid writing log
+    'output_headslog' : 'log/output_heads.log', # Set to '' to avoid writing log
+    'link_maplog'     : 'log/link_map.log',     # Set to '' to avoid writing log
+    'link_statslog'   : 'log/link_stats.log',   # Set to '' to avoid writing log
+    'link_headslog'   : 'log/link_heads.log'    # Set to '' to avoid writing log
 }
 
-PATH_MAPPINGS = {
+PATH_MAPPINGS = { # Set path mappings; used by `create_links` functions
     'root': ROOT
 }
 
-# CONFIG USER
+### CONFIG USER 
 config_user = yaml.load(open(PATHS['config_user'], 'rb'))
-if config_user['local']['executables']:
-    gs.private.metadata.default_executables[os.name].update(config_user['local']['executables']) 
-if config_user['external']:
+if config_user['local']['executables']: # Update executables; used by `run_program` functions
+    gs.private.metadata.default_executables[os.name].update(config_user['local']['executables'])
+if config_user['external']: # Update path mappings; used by `create_links` functions
     PATH_MAPPINGS.update(config_user['external'])
 
-# START
+### START 
 gs.remove_dir(['input', 'external'])
 gs.clear_dir(['output', 'log'])
 gs.start_makelog(PATHS)
 
-# GET INPUT FILES
+### GET INPUT FILES 
 inputs = gs.create_input_links(PATHS, ['inputs.txt'], PATH_MAPPINGS)
 externals = gs.create_external_links(PATHS, ['externals.txt'], PATH_MAPPINGS)
 gs.write_link_logs(PATHS, inputs + externals)
+gs.get_modified_links(PATHS, inputs + externals)
 
-# RUN SCRIPTS
+### RUN SCRIPTS
 gs.run_r(PATHS, program = 'code/create_table_data.r')
 gs.run_stata(PATHS, program = 'code/create_graph_data.do')
 
-# LOG OUTPUTS
+### LOG OUTPUTS
 gs.log_files_in_output(PATHS)
 
-# END
+### END
 gs.end_makelog(PATHS)
-raw_input('\n Press <Enter> to exit.')
