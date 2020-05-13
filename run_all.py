debc72c48e905221573c437d7fb3378c2866ed8b
Zong Huang <40323298+z-y-huang@users.noreply.github.com>
2020-05-12 19:43:31 -0700

    Add executable import to run_all

--- run_all.py
+++ run_all.py
@@ -4,12 +4,22 @@
 import git
 import imp
 import os
-import yaml
 
-# LOAD GSLAB MAKE
+### SET DEFAULT PATHS
 ROOT = git.Repo('.', search_parent_directories = True).working_tree_dir 
-f, path, desc = imp.find_module('gslab_make', [os.path.join(ROOT, 'lib')]) 
+
+PATHS = {
+    'root'        : ROOT,
+    'lib'         : os.path.join(ROOT, 'lib'),
+    'config_user' : os.path.join(ROOT, 'config_user.yaml')
+}
+
+### LOAD GSLAB MAKE
+f, path, desc = imp.find_module('gslab_make', [PATHS['lib']]) 
 gs = imp.load_module('gslab_make', f, path, desc)
+
+### LOAD CONFIG USER 
+gs.update_executables(PATHS)
 
 ###########
 ### RUN ###
