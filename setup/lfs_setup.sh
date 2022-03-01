b7ba1c279dd01f06a39faf2da286938585a5f042
Saam Zahedian <snzahedian@gmail.com>
2022-03-01 12:16:38 -0800

    #51 Modify .gitattributes and create lfs setup script

---
+++ setup/lfs_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git lfs track "*.csv"
+git lfs track "*.dta"
+git lfs track "*.rda"
+git lfs track "*.rds"
+git lfs track "*.pdf"
+git lfs track "*.png"
+git lfs track "*.zip"
+git lfs track "*.tar.gz"
+git lfs track "*.csv"
+
+git add .gitattributes
