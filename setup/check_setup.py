b3ed716047016d3eba7ff058b374d3d04ed5066d
Sameer Nair-Desai <63830369+snairdesai@users.noreply.github.com>
2022-08-19 14:50:42 -0700

    Updated check_setup.py to include robustness checks of core dependencies for non-conda users, plus added instructions on manual installs.
    Follows from Issue #18 in GS-Lab RA Manual.

--- setup/check_setup.py
+++ setup/check_setup.py
@@ -12,6 +12,7 @@ try:
     from termcolor import colored
     import colorama
     colorama.init()
+
 except:
     print("Please ensure that conda env is activated and that yaml,")
     print("termcolor, and colorama are in conda_env.yaml")
@@ -28,6 +29,25 @@ format_message = gs.private.utility.format_message
 
 
 # GENERAL FUNCTIONS
+def read_env_yaml():
+    with open('conda_env.yaml', 'r') as file:
+        packages = yaml.safe_load(file)
+    return packages
+
+
+def get_python(packages):
+    prefixes_python = ('python', 'python=', "pip=", "r=", "r-", "r", "pyyaml", "gitpython")
+    python_packages = [item for item in packages['dependencies'] if not 
+                       item.startswith(prefixes_python)]
+    return python_packages
+
+
+def get_r(packages):
+    prefixes_r = ("r-")
+    r_packages = [item for item in packages['dependencies'] if item.startswith(prefixes_r)] 
+    return r_packages
+
+
 def parse_yaml_files(config = '../config.yaml', config_user = '../config_user.yaml'):
     if not os.path.isfile(config_user):
         shutil.copy('config_user_template.yaml', config_user)
@@ -81,10 +101,38 @@ def check_external_paths(config_user):
                 raise gs.private.exceptionclasses.ColoredError(error_message)
 
 
+def check_dependencies():
+    try:
+        import importlib
+        packages = read_env_yaml()
+        python_packages = get_python(packages)
+        r_packages = get_r(packages)
+        for package in python_packages:
+            globals()[package] = importlib.import_module(package)
+        for package in r_packages:
+            package = package.replace('r-',"")
+            output=str(subprocess.run(['Rscript', '-e', f'library("{package}")']))
+            if "returncode=0" not in output:
+                error_message = 'ERROR! R package `%s` is not installed.' % package
+                error_message = format_message(error_message)
+                raise gs.private.exceptionclasses.ColoredError(error_message)
+        if 'pyyaml' in python_packages:
+            import yaml
+        if 'gitpython' in python_packages:
+            import git
+            
+    except:
+        error_message = "Please ensure that you have the correct conda environment activated.\nIf you are not using conda, ensure you have installed all dependencies from dependencies.txt."
+        error_message = format_message(error_message)
+        raise gs.private.exceptionclasses.ColoredError(error_message)
+        raise Exception
+
+
 def configuration():
     (config, config_user) = parse_yaml_files()
     check_software(config, config_user)
     check_external_paths(config_user)
+    check_dependencies()
     message = format_message('SUCCESS! Setup complete.')
     print(colored(message, 'green'))
 
