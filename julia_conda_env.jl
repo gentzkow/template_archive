3f31ec8ea016f759829be13ff4e366c726c606ba
jccisneros <jccp@stanford.edu>
2022-10-13 13:33:36 -0700

    Change Julia environment building process

---
+++ julia_conda_env.jl
@@ -0,0 +1,12 @@
+### Install Julia packages in the conda environment ###
+### The default is to use the latest version of each of the Julia packages required.
+### Comment the latest version of the package that was used in a full run. 
+
+using Pkg
+Pkg.add("DataFrames") # v1.4.1
+Pkg.add("CSV") # v0.10.4
+Pkg.add("StatsPlots") # v0.15.4
+Pkg.add("PyPlot") # v2.11.0
+
+# Check the version of the Julia packages
+#Use Pkg.status()
