bf64d72c8ac90175c46c14176e5174138d1b1e18
jccisneros <jccp@stanford.edu>
2022-10-14 18:45:59 -0700

    #68 Set Python path

--- julia_conda_env.jl
+++ julia_conda_env.jl
@@ -1,3 +1,10 @@
+#######################
+### JULIA ENV SETUP ###
+#######################
+
+# Set the path to the correct Python installation
+ENV["PYTHON"]="/usr/local/Caskroom/miniconda/base/envs/template_julia/bin/python"
+
 ### Install Julia packages in the conda environment ###
 ### The default is to use the latest version of each of the Julia packages required.
 ### Comment the latest version of the package that was used in a full run. 
