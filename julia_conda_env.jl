8b1c26b6f6a3e1b0c61972facb033fcd8c2600a0
jc-cisneros <jccp@stanford.edu>
2022-10-18 00:33:31 +0000

    #68 Full run on Docker Debian container

--- julia_conda_env.jl
+++ julia_conda_env.jl
@@ -2,20 +2,14 @@
 ### JULIA ENV SETUP ###
 #######################
 
-# Set the path to the correct Python installation
-ENV["PYTHON"]="/usr/local/Caskroom/miniconda/base/envs/template_julia/bin/python"
-
 ### Install Julia packages in the conda environment ###
 ### The default is to use the latest version of each of the Julia packages required.
 ### Comment the latest version of the package that was used in a full run. 
 
 using Pkg
-Pkg.add("Conda") # v1.4.1
-Pkg.add("PyCall") # v1.4.1
-Pkg.add("DataFrames") # v1.4.1
-Pkg.add("CSV") # v0.10.4
-Pkg.add("StatsPlots") # v0.15.4
-Pkg.add("PyPlot") # v2.11.0
+Pkg.add("Conda")
+using Conda
+Pkg.add(["PyCall", "DataFrames", "CSV", "StatsPlots", "PyPlot"])
 
 # Check the version of the Julia packages
 #Use Pkg.status()
